e945ca121b9440d02bcfd2495d65ca99eecca1ee
coding815 <coding815@gmail.com>
2021-11-16 17:53:49 +0900

    first commit

---
+++ exLotto.java
@@ -0,0 +1,10 @@
+package exLotto;
+
+public class exLotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("exLotto: " + "exLotto!!!");
+	}
+
+}
